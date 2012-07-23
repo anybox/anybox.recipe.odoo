6bbd8ce1790ab99c44902468dff483214ffabb31
Georges Racinet <gracinet@purity>
2012-07-23 18:15:53 +0200

    Started writing tests for main openerp version parsing

--- anybox/recipe/openerp/tests/test_base.py
+++ anybox/recipe/openerp/tests/test_base.py
@@ -5,6 +5,13 @@ import shutil
 from tempfile import mkdtemp
 from anybox.recipe.openerp.server import BaseRecipe
 
+
+class TestingRecipe(BaseRecipe):
+    """A subclass with just enough few defaults for unit testing."""
+
+    archive_filenames = {'6.1': '6-1-blob-%s'}
+    archive_nightly_filenames = {'6.1': '6-1-nightly-%s'}
+
 class TestBaseRecipe(unittest.TestCase):
 
     def setUp(self):
@@ -20,5 +27,39 @@ class TestBaseRecipe(unittest.TestCase):
     def tearDown(self):
         shutil.rmtree(self.buildout_dir)
 
-    def test_parse_version(self):
-        pass
+    def make_recipe(self, name='openerp', **options):
+        recipe = self.recipe = TestingRecipe(self.buildout, name, options)
+
+    def test_correct_v_6_1(self):
+        self.make_recipe(version='6.1')
+        self.assertEquals(self.recipe.version_wanted, '6.1-1')
+
+    def test_version_release_6_1(self):
+        self.make_recipe(version='6.1-1')
+
+        recipe = self.recipe
+        self.assertEquals(recipe.version_wanted, '6.1-1')
+        self.assertEquals(recipe.type, 'downloadable')
+        self.assertEquals(
+            recipe.url,
+            'http://nightly.openerp.com/6.1/releases/6-1-blob-6.1-1')
+
+    def test_version_nightly_6_1(self):
+        self.make_recipe(version='nightly 6.1 1234-5')
+
+        recipe = self.recipe
+        self.assertEquals(recipe.type, 'downloadable')
+        self.assertEquals(
+            recipe.url,
+            'http://nightly.openerp.com/6.1/nightly/src/6-1-nightly-1234-5')
+
+    def test_version_bzr_6_1(self):
+        self.make_recipe(
+            version='bzr lp:openobject-server/6.1 openerp-6.1 last:1')
+
+        recipe = self.recipe
+        self.assertEquals(recipe.type, 'bzr')
+        self.assertEquals(recipe.url, 'lp:openobject-server/6.1')
+        self.assertEquals(recipe.openerp_dir,
+                          os.path.join(recipe.parts, 'openerp-6.1'))
+
