773af4e489b0dc9be37c13d67621ad5ab04aaf2c
Georges Racinet <gracinet@anybox.fr>
2013-07-26 12:53:22 +0200

    Excluding dirty test files from oerp60 from flake8

--- anybox/recipe/openerp/tests/oerp60/bin/release.py
+++ anybox/recipe/openerp/tests/oerp60/bin/release.py
@@ -1,4 +1,5 @@
 # -*- encoding: utf-8 -*-
+# flake8: noqa
 ##############################################################################
 #
 #    OpenERP, Open Source Management Solution
--- anybox/recipe/openerp/tests/oerp60/setup.py
+++ anybox/recipe/openerp/tests/oerp60/setup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# flake8: noqa
 ##############################################################################
 #
 #    OpenERP, Open Source Management Solution
