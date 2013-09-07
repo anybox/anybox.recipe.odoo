07ffe35b71b4e11241d89de748e8d5d417377b8c
Georges Racinet <gracinet@anybox.fr>
2013-09-07 11:25:18 +0200

    Fixing version info for release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 import sys
 from setuptools import setup, find_packages
 
-version = '1.7.1dev'
+version = '1.7.1'
 
 if sys.version_info < (2, 6):
     sys.stderr.write("This package requires Python 2.6 or newer. "
