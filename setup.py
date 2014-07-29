bc20fccd10686c31c40f6c3709aaf255207bd7a9
Georges Racinet <gracinet@ishtar.anybox.fr>
2014-07-29 11:06:54 +0200

    dev version was wrong

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 import sys
 from setuptools import setup, find_packages
 
-version = '1.8.4'
+version = '1.8.5'
 
 if sys.version_info < (2, 6):
     sys.stderr.write("This package requires Python 2.6 or newer. "
