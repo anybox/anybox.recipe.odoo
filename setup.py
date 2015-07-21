a15b02b45d58a723d452bf9f26ff7cae6c92654a
Georges Racinet <gracinet@anybox.fr>
2015-07-21 18:55:08 +0200

    Pinning to pip 1.5.6 to get a clearer picture

--- setup.py
+++ setup.py
@@ -11,7 +11,8 @@ if sys.version_info < (2, 6):
 
 # a sufficient version of pip is needed to parse Odoo requirement file
 # version 1.4.1 is the one required by reportlab anyway
-requires = ['setuptools', 'zc.recipe.egg', 'zc.buildout>=2.2.0', 'pip>=1.4.1']
+# (temporary fixing to 1.5.6 to stop conflating of issues)
+requires = ['setuptools', 'zc.recipe.egg', 'zc.buildout>=2.2.0', 'pip==1.5.6']
 
 if sys.version_info < (2, 7):
     requires.append('ordereddict')
